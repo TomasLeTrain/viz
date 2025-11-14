27a12871114bc380e203a76a7e6cbaf0a2ca83a8
TomasLeTrain <tomylopezd@gmail.com>
2025-11-14 17:52:07 -0500

    rough sidebar impl

--- main.cpp
+++ main.cpp
@@ -24,11 +24,13 @@
 #include <QStackedWidget>
 #include <QStyleOption>
 #include <QTimer>
+#include <QToolButton>
 #include <QVBoxLayout>
 #include <QWheelEvent>
 #include <algorithm>
 #include <iostream>
 #include <qgesture.h>
+#include <qmainwindow.h>
 #include <qnamespace.h>
 
 class FieldView : public QGraphicsView {
@@ -143,13 +145,150 @@ private:
   QVector<QGraphicsPathItem *> paths;
 };
 
+// card in sidebar
+class ComponentCard : public QFrame {
+  Q_OBJECT
+public:
+  ComponentCard(QString id, QString title, int priority,
+                QWidget *parent = nullptr)
+      : QFrame(parent), id(id), priority(priority) {
+
+    setFrameShape(QFrame::StyledPanel);
+    setFrameShadow(QFrame::Plain);
+
+    // contains title then info
+    auto *lay = new QVBoxLayout(this);
+    // contains title and remove box
+    auto *h = new QHBoxLayout;
+
+    QLabel *title_label = new QLabel(QString("<b>%1</b>").arg(title));
+    expand_collapse_btn = new QToolButton;
+
+    expand_collapse_btn->setToolButtonStyle(Qt::ToolButtonIconOnly);
+    expand_collapse_btn->setArrowType(Qt::ArrowType::UpArrow);
+
+    // TODO: expand across the entire title so its a lot easier to access
+    expand_collapse_btn->setFixedWidth(20);
+
+    connect(expand_collapse_btn, &QPushButton::clicked, this, &ComponentCard::expand_collapse);
+
+    h->addWidget(title_label);
+    h->addStretch();
+    h->addWidget(expand_collapse_btn);
+
+    lay->addLayout(h);
+
+    info = new QLabel(QString("info... %1").arg(priority));
+    info->setWordWrap(true);
+
+    lay->addWidget(info);
+  }
+
+  QString getId() const { return id; }
+  int getPriority() const { return priority; }
+  void setPriority(int p) { priority = p; }
+  void setInfoText(const QString &s) { info->setText(s); }
+
+signals:
+  void removeRequested(const QString &id);
+
+private slots:
+  void requestRemove() { emit removeRequested(id); }
+  void expand_collapse() {
+    if (info->isVisible()) {
+      info->setVisible(false);
+      expand_collapse_btn->setArrowType(Qt::ArrowType::DownArrow);
+      // remove_btn->setText(QString("Expand"));
+    } else {
+      info->setVisible(true);
+      expand_collapse_btn->setArrowType(Qt::ArrowType::UpArrow);
+      // remove_btn->setText(QString("Collapse"));
+    }
+  }
+
+private:
+  QToolButton *expand_collapse_btn;
+  QString id;
+  int priority;
+  QLabel *info;
+};
+
+// ----------------------
+// Sidebar manager
+// ----------------------
 class Sidebar : public QWidget {
   Q_OBJECT
-
 public:
-  Sidebar() {
-    // TODO
+  Sidebar(QWidget *parent = nullptr) : QWidget(parent) {
+    auto *lay = new QVBoxLayout(this);
+    setMinimumWidth(260);
+    scroll = new QScrollArea;
+    scroll->setWidgetResizable(true);
+    container = new QWidget;
+    vlay = new QVBoxLayout(container);
+    vlay->addStretch();
+    scroll->setWidget(container);
+    lay->addWidget(scroll);
+    QPushButton *add = new QPushButton("Add random component");
+    connect(add, &QPushButton::clicked, this, &Sidebar::addRandomComponent);
+    lay->addWidget(add);
   }
+
+  void addComponent(const QString &id, const QString &title, int priority) {
+    ComponentCard *card = new ComponentCard(id, title, priority);
+    connect(card, &ComponentCard::removeRequested, this,
+            &Sidebar::removeComponentById);
+    cards.push_back(card);
+    refreshOrder();
+  }
+
+public slots:
+  void addRandomComponent() {
+    static int n = 1;
+    QString id = QString("comp_%1").arg(n++);
+    int p = rand() % 100;
+    addComponent(id, QString("Component %1").arg(id), p);
+  }
+
+  void removeComponentById(const QString &id) {
+    auto it = std::find_if(cards.begin(), cards.end(),
+                           [&](ComponentCard *c) { return c->getId() == id; });
+    if (it != cards.end()) {
+      ComponentCard *c = *it;
+      cards.erase(it);
+      vlay->removeWidget(c);
+      c->deleteLater();
+      refreshOrder();
+    }
+  }
+
+  // update the priority and reorder
+  void refreshOrder() {
+    // sort cards by priority descending
+    std::sort(cards.begin(), cards.end(),
+              [](ComponentCard *a, ComponentCard *b) {
+                return a->getPriority() > b->getPriority();
+              });
+
+    // clear existing widgets from layout
+    QLayoutItem *child;
+
+    // removes all children
+    while ((child = vlay->takeAt(0)) != nullptr)
+      ;
+
+    for (ComponentCard *c : cards) {
+      vlay->addWidget(c);
+    }
+    vlay->addStretch();
+    container->setLayout(vlay);
+  }
+
+private:
+  QScrollArea *scroll;
+  QWidget *container;
+  QVBoxLayout *vlay;
+  QVector<ComponentCard *> cards;
 };
 
 // ----------------------
@@ -168,6 +307,9 @@ public:
 
     // separates field from timeline
     QSplitter *fieldSplit = new QSplitter(Qt::Vertical);
+
+    mainSplit->setHandleWidth(10);
+    fieldSplit->setHandleWidth(10);
 
     fieldView = new FieldView;
     fieldSplit->addWidget(fieldView);
@@ -227,9 +369,6 @@ private:
   Sidebar *sidebar;
 };
 
-// ----------------------
-// Main window tying everything together
-// ----------------------
 class MainWindow : public QMainWindow {
   Q_OBJECT
 
@@ -242,14 +381,32 @@ public:
     stacked->addWidget(fieldWindow);
 
     setCentralWidget(stacked);
-
-    setWindowTitle("Viz");
   }
 
 private:
   QStackedWidget *stacked;
   FieldWindow *fieldWindow;
 };
+
+void add_path(){
+    // TODO: impl
+    // add to field
+    // add info to sidebar
+    // add to some list that keeps tracks of all elements
+}
+
+void add_point_cloud(){
+    // TODO: impl
+    // add to field
+    // add info to sidebar
+    // add to some list that keeps tracks of all elements
+}
+
+void load_file(){
+    // TODO: impl
+    // load field image?
+    // load paths / point clouds
+}
 
 int main(int argc, char **argv) {
   QApplication app(argc, argv);
